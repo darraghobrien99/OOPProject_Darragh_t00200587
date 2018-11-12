48d679a9fc3d07f5022722f7fd298d85fee86ca8
darraghobrien99 <darraghmobrien@hotmail.com>
2018-11-12 20:20:06 +0000

    Edited Question Class/ Removed Answer Class Removed code from quiz and edited it

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,48 +1,53 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Quiz {
     public static void main(String args[]) {
 
-        Question[] questions = new Question[10];
-        String[] answers = {"B", "C","C","A","C","B","A","A","C","A"};
-        int correct=0;
-        int wrong=0;
-        String userAnswer = "";
+        String question;
+        String correctAnswer;
+        String wrongAnswer1;
+        String wrongAnswer2;
+        String wrongAnswer3;
+        int number;
 
-        for(int i=0;i<questions.length;i++)
-        {
-            Question Question1 = new Question();
-            Question Question2 = new Question();
+        number = Integer.parseInt(JOptionPane.showInputDialog("How many questions would you like to add?"));
 
-            Question1.setQuestions(JOptionPane.showInputDialog("What is the capital of USA?\n" +
-                    "\nA. Washington DC " +
-                    "\nB. Chicago " +
-                    "\nC. New York"));
+        ArrayList<Question> QuizQs =new ArrayList<>();
+        Question[] questions =new Question[number];
+        Question question1;
 
-            Question2.setQuestions(JOptionPane.showInputDialog("What Color is Cobalt?\n" +
-                    "\nA.Red" +
-                    "\nB.Silver" +
-                    "\nC.Blue"));
+        /**
+        for(int i=0; i<number;i++){
 
-            questions[i] = Question1;
-            questions[i] = Question2;
+            question = JOptionPane.showInputDialog("Enter Question:");
+            correctAnswer = JOptionPane.showInputDialog("Enter correct Answer:");
+            wrongAnswer1 = JOptionPane.showInputDialog("Enter wrong answer 1:");
+            wrongAnswer2 = JOptionPane.showInputDialog("Enter wrong answer 2:");
+            wrongAnswer3 = JOptionPane.showInputDialog("Enter wrong answer 3:");
 
-            break;
+            question1 = new Question(question, correctAnswer, wrongAnswer1, wrongAnswer2,wrongAnswer3);
+
+           questions[i] = question1;
         }
 
-        for(int i=0; i<answers.length;i++)
-        {
-            if (answers[i].equals(userAnswer)) {
+*/
 
-                correct++;
-            }
+        question1 = new Question("What is the most spoken language in the world?", "Modern Chinese", "Spanish", "English", "French");
 
-            else
-                wrong++;
-        }
+        QuizQs.add(question1);
+        questions[0] = question1;
 
-        JOptionPane.showMessageDialog(null,"You got " + correct + " correct");
+        question1 = new Question("Where would you find the Sea of Tranquility?", "The Moon", "Iceland", "Germany", "Mars");
+
+        QuizQs.add(question1);
+        questions[1] = question1;
+
+
+
+
+
     }
 
 
-}
+    }
