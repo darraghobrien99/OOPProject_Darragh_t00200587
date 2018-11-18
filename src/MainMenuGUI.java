7c2b311cc7887195c5cb7cc2ebce96471c6f0130
darraghobrien99 <darraghmobrien@hotmail.com>
2018-11-18 18:37:00 +0000

    More GUI work

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.Timer;
 
 public class MainMenuGUI extends JFrame implements ActionListener {
 
@@ -14,6 +15,8 @@ public class MainMenuGUI extends JFrame implements ActionListener {
     JLabel heading;
     JLabel question;
     JButton[] buttons;
+    Timer timer = null;
+    int seconds = 60;
 
 
     public MainMenuGUI() {
@@ -22,7 +25,7 @@ public class MainMenuGUI extends JFrame implements ActionListener {
 
         super("10 Question Quiz");
         getContentPane().setLayout(null);
-        setSize(400,400);
+        setSize(800,550);
         setBackground(Color.GRAY);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -30,17 +33,17 @@ public class MainMenuGUI extends JFrame implements ActionListener {
         //panel
 
         change = new JPanel(null);
-        FlowLayout flow = new FlowLayout();
-        getContentPane().setLayout(flow);
         change.setBackground(Color.GRAY);
+        change.setBounds(0,0,800,800);
         getContentPane().add(change);
 
         // Heading
 
         heading = new JLabel("Test Your Knowledge");
-        heading.setForeground(Color.BLUE);
-        heading.setFont(new Font("Tahoma", Font.BOLD, 20));
-        getContentPane().add(heading);
+        heading.setForeground(new Color(59, 89, 182));
+        heading.setFont(new Font("Tahoma", Font.BOLD, 40));
+        heading.setBounds(160,0,700,200);
+        change.add(heading);
 
         //play button
 
@@ -49,7 +52,8 @@ public class MainMenuGUI extends JFrame implements ActionListener {
         playButton.setForeground(Color.WHITE);
         playButton.setBackground(new Color(59, 89, 182));
         playButton.setFont(new Font("Tahoma", Font.BOLD, 12));
-        getContentPane().add(playButton);
+        playButton.setBounds(170,350,100,100);
+        change.add(playButton);
 
         //score button
 
@@ -58,7 +62,8 @@ public class MainMenuGUI extends JFrame implements ActionListener {
         scoresButton .setForeground(Color.WHITE);
         scoresButton .setBackground(new Color(59, 89, 182));
         scoresButton .setFont(new Font("Tahoma", Font.BOLD, 12));
-        getContentPane().add(scoresButton);
+        scoresButton.setBounds(270,350,200,100);
+        change.add(scoresButton);
 
         //exit Button
 
@@ -67,7 +72,8 @@ public class MainMenuGUI extends JFrame implements ActionListener {
         exitButton.setForeground(Color.WHITE);
         exitButton.setBackground(new Color(59, 89, 182));
         exitButton.setFont(new Font("Tahoma", Font.BOLD, 12));
-        getContentPane().add(exitButton);
+        exitButton.setBounds(470,350,100,100);
+        change.add(exitButton);
 
         playButton.addActionListener(this);
         scoresButton.addActionListener(this);
@@ -92,18 +98,44 @@ public class MainMenuGUI extends JFrame implements ActionListener {
     }
 
    public void startQuiz() {
-        GridLayout grid = new GridLayout();
-        change = new JPanel(grid);
-        change.setBounds(0,0,400,400);
+        change = new JPanel(null);
+        change.setBounds(0,0,800,800);
         change.setBackground(Color.GRAY);
 
+        buttons = new JButton[4];
+
+
+        for(int i=0; i< buttons.length;i++){
+
+            buttons[i] = new JButton(" ");
+            buttons[i].setBounds(0,0,200,100);
+            buttons[i].setFocusPainted(false);
+            buttons[i].setForeground(Color.WHITE);
+            buttons[i].setBackground(new Color(59, 89, 182));
+            buttons[i].setFont(new Font("Tahoma", Font.BOLD, 12));
+            buttons[i].setBounds(470,350,100,100);
+
+            change.add(buttons[i]);
+        }
+
+        timer = new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if()
+            }
+        });
+
+
+
+
+/*
        QuizQuestion quizQuestion = new QuizQuestion();
 
        for (Question q : quizQuestion.getQuizQuestions()) {
            System.out.println(quizQuestion);
        }
 
-       /*
+
        //create buttons for answers
         buttons = new JButton[4];
 
